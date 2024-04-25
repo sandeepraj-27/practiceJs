9daf8f5817e9008750c37f61a1ee0c97b601bb8d
sandeepraj-27 <159559069+sandeepraj-27@users.noreply.github.com>
2024-04-25 16:48:55 +0000

    conversion

--- 01_basic/test.js
+++ 01_basic/test.js
@@ -1,1 +1,22 @@
 console.log("sandeep");
+let a=1;
+let b;
+"use strict"; //treat all js code as newer version
+console.table([a,b]);
+
+
+// conversion
+let score ;
+let valueinNumber=Number(score);
+console.log(typeof score);
+console.log(typeof valueinNumber);
+console.log(valueinNumber);
+/*
+"33"=>33
+"33abc"=>nan
+true=1;false=0;
+null=>0;
+undefined=>NAN;
+
+
+*/
--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # practiceJs
 practice js repo
+/*
+let a=1;
+let b;
+"use strict"; //treat all js code as newer version
+console.table([a,b]);
+*/
+//typeof "sandeep"; //string
+//typeof null; //object
+//typeof undefined;  undefined
